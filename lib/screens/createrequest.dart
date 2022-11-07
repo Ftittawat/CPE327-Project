57f0aa8bee926a742e75fe95cf3b8324992422bc
StanG12 <90758882+StanG12@users.noreply.github.com>
2022-11-08 02:10:58 +0700

    Map edit

--- lib/screens/createrequest.dart
+++ lib/screens/createrequest.dart
@@ -2,10 +2,11 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CreateRequest extends StatefulWidget {
   const CreateRequest({super.key});
@@ -208,13 +209,38 @@ class _CreateRequestState extends State<CreateRequest> {
     );
   }
 
+  //Map field
+  Completer<GoogleMapController> _controller = Completer();
+  late LocationData currentLocation;
+
+  Future<LocationData?> getCurrentLocation() async {
+    Location location = Location();
+    try {
+      return await location.getLocation();
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        // Permission denied
+      }
+      return null;
+    }
+  }
+
+  Future _goToMe() async {
+    final GoogleMapController controller = await _controller.future;
+    currentLocation = (await getCurrentLocation())!;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+      zoom: 16,
+    )));
+  }
+
   Widget mapBox() {
     LatLng latLng = LatLng(13.651158, 100.496454);
     CameraPosition cameraPosition = CameraPosition(
       target: latLng,
       zoom: 16.0,
     );
-    Completer<GoogleMapController> _controller = Completer();
+
     return Container(
       height: 180,
       decoration: BoxDecoration(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import cloud_firestore
 import firebase_core
 import firebase_database
+import location
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
