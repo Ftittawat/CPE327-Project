faafb5ad71f8332b6471500f5a8c718e5ab8d82d
Passakorn Klaikaew <passakornjiew@gmail.com>
2022-11-20 16:28:01 +0700

    login only with google

--- lib/components/authen_service.dart
+++ lib/components/authen_service.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:helpee/models/user_models.dart';
 
 Future<Null> findNameAndEmail() async {
   await Firebase.initializeApp().then((value) async {
@@ -12,3 +15,65 @@ Future<Null> findNameAndEmail() async {
     });
   });
 }
+
+// Future<Null> processSignInGoogle() async {
+//   late String email, password, name, uid;
+//   print(" # Logging in ... #");
+//   GoogleSignIn _googleSignIn = GoogleSignIn(
+//     scopes: [
+//       'email',
+//       'https://www.googleapis.com/auth/contacts.readonly',
+//     ],
+//   );
+
+//   await Firebase.initializeApp().then((value) async {
+//     await _googleSignIn.signIn().then((value) async {
+//       name = value!.displayName!;
+//       email = value.email;
+
+//       await value.authentication.then((value2) async {
+//         AuthCredential authCredential = GoogleAuthProvider.credential(
+//           idToken: value2.idToken,
+//           accessToken: value2.accessToken,
+//         );
+//         await FirebaseAuth.instance
+//             .signInWithCredential(authCredential)
+//             .then((value3) async {
+//           uid = value3.user!.uid;
+//           print(
+//               " # Login With Gmail Success With name = $name, email = $email, uid =$uid #");
+//           insertValueToCloudFirestore();
+
+//           // await FirebaseFirestore.instance
+//           //     .collection('user')
+//           //     .doc(uid)
+//           //     .snapshots()
+//           //     .listen((event) {
+//           //   print('event ==> ${event.data()}');
+//           //   if (event.data() == null) {
+//           //     // Call TypeUser
+//           //   } else {
+//           //     // Route to Service by TypeUser
+//           //   }
+//           // });
+//           Navigator.pop(context);
+//         });
+//       });
+//     });
+//   });
+// }
+
+// Future<Null> insertValueToCloudFirestore() async {
+//   UserModel user = UserModel(email: email, name: name);
+//   Map<String, dynamic> data = user.toMap();
+//   await Firebase.initializeApp().then((value) async {
+//     await FirebaseFirestore.instance
+//         .collection('user')
+//         .doc(uid)
+//         .set(data)
+//         .then((value) {
+//       print(' # Insert Value To Firestore Success Email= $email #');
+//       // Navigator
+//     });
+//   });
+// }
