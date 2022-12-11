3fa46c22e9cf7f8cad7810d2773142ab2499a76d
StanG12 <90758882+StanG12@users.noreply.github.com>
2022-12-11 23:30:02 +0700

    Distance Fix

--- lib/widgets/displayAllRequest.dart
+++ lib/widgets/displayAllRequest.dart
@@ -14,7 +14,7 @@ Future<Null> findLatLng() async {
   LocationData? locationData = await getCurrentLocation();
   lat = locationData?.latitude;
   lng = locationData?.longitude;
-  print('lat = $lat lng = $lng');
+  //print('lat = $lat lng = $lng');
 }
 
 LocationData? currentLocation;
@@ -48,9 +48,10 @@ Widget displayAllRequest() {
                   var data =
                       snapshot.data!.docs[index].data() as Map<String, dynamic>;
                   var dis = Geolocator.distanceBetween(
-                          data['Lat'], data['Lng'], lat!, lng!)
-                      .toStringAsFixed(0);
-                  print('dis = $dis');
+                          data['Lat'], data['Lng'], lat!, lng!) /
+                      1000;
+                  var disKm = dis.toStringAsFixed(2);
+                  //print('dis in Km = $disKm');
 
                   // Convert Timestamp to DateTime
                   DateTime? dateTime;
@@ -111,7 +112,7 @@ Widget displayAllRequest() {
                                       ),
                                       /* ----------------- Distance Text ---------------- */
                                       Text(
-                                        "Distance $dis kilometers.",
+                                        "Distance $disKm kilometers.",
                                         /* ***** */
                                         style: TextStyle(
                                           fontSize: 13,
@@ -243,7 +244,7 @@ Widget displayAllRequest() {
                                     /* ----------------- Distance Text ---------------- */
 
                                     Text(
-                                      "Distance $dis kilometers.",
+                                      "Distance $disKm kilometers.",
                                       style: TextStyle(
                                         fontSize: 13,
                                         fontWeight: FontWeight.w400,
