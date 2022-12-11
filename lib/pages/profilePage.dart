b27ba47e9b3ec42e88a6e9f955f28997a9ddefa9
stemG <77492060+gkana19@users.noreply.github.com>
2022-12-12 02:47:07 +0700

    Update profilePage.dart

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -26,22 +26,6 @@ class _ProfileState extends State<Profile> {
     super.initState();
     print("--- ### Proflie ### ---");
     loginCheck();
-    // getData();
-  }
-
-  Future<Null> getData() async {
-    // enter here the path , from where you want to fetch the doc
-    print(user!.uid);
-    DocumentSnapshot pathData = await FirebaseFirestore.instance
-        .collection('user')
-        .doc(user!.uid)
-        .get();
-
-    if (pathData.exists) {
-      Map<String, dynamic>? fetchDoc = pathData.data() as Map<String, dynamic>?;
-      var email = fetchDoc?['email'];
-      print(email);
-    }
   }
 
   Future<Null> loginCheck() async {
@@ -86,15 +70,13 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  Widget emailBox() {
+  Widget emailBox(String email) {
     return TextField(
       style: GoogleFonts.montserrat(
           fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
       decoration: InputDecoration(
         enabled: false,
-        labelText: FirebaseAuth.instance.currentUser?.email ?? 'Email',
-        labelStyle: GoogleFonts.montserrat(
-            fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+        labelText: email,
         disabledBorder: OutlineInputBorder(
             borderSide: BorderSide(width: 1.0, color: Colors.grey.shade400),
             borderRadius: BorderRadius.circular(10)),
@@ -103,17 +85,13 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  Widget phoneBox() {
+  Widget phoneBox(String phoneNumber) {
     return TextField(
       keyboardType: TextInputType.phone,
       style: GoogleFonts.montserrat(
           fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
       decoration: InputDecoration(
-          // hintText: 'Phone',
-          // hintStyle: GoogleFonts.montserrat(
-          //     fontSize: 16,
-          //     fontWeight: FontWeight.w600,
-          //     color: Colors.grey.shade400),
+          labelText: phoneNumber,
           contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
           errorBorder: OutlineInputBorder(
               borderSide: BorderSide(width: 1.0, color: Colors.red.shade400),
@@ -132,16 +110,12 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  Widget otherContactsBox() {
+  Widget otherContactsBox(String? othersContact) {
     return TextField(
       style: GoogleFonts.montserrat(
           fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
       decoration: InputDecoration(
-          // hintText: 'Phone',
-          // hintStyle: GoogleFonts.montserrat(
-          //     fontSize: 16,
-          //     fontWeight: FontWeight.w600,
-          //     color: Colors.grey.shade400),
+          labelText: (othersContact == "") ? "" : othersContact,
           contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
           errorBorder: OutlineInputBorder(
               borderSide: BorderSide(width: 1.0, color: Colors.red.shade400),
@@ -242,7 +216,6 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -281,89 +254,99 @@ class _ProfileState extends State<Profile> {
         ),
         body: SingleChildScrollView(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                /* ----------------- Prifile Image ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey.shade400,
-                    radius: 80,
-                    backgroundImage: NetworkImage(
-                        FirebaseAuth.instance.currentUser?.photoURL ??
-                            "assets/images/Memoji.png"),
-                    // backgroundImage: NetworkImage(
-                    //     user?.photoURL! ?? "assets/images/Memoji.png"),
-                  ),
-                ),
-                /* ----------------- Username ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Text(
-                      FirebaseAuth.instance.currentUser?.displayName ??
-                          'DisplayName',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 27,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black)),
-                ),
-                /* ----------------- Address ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 5, 20, 0),
-                  child: Text("Thung khru, Bangkok.",
-                      style: GoogleFonts.montserrat(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black)),
-                ),
-                /* ----------------- Show Email ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: Column(
-                    children: [
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: textLabel('Email')),
-                      emailBox(),
-                    ],
-                  ),
-                ),
-                /* ----------------- Edit Phone ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: Column(
-                    children: [
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: textLabel('Phone')),
-                      phoneBox(),
-                    ],
-                  ),
-                ),
-                /* ----------------- Edit Other Contact ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: Column(
-                    children: [
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: textLabel('Other Contact (Optional)')),
-                      otherContactsBox(),
-                    ],
-                  ),
-                ),
-                /* ----------------- Save Button ---------------- */
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 150),
-                  child: SizedBox(
-                    height: 55.0,
-                    child: saveButton(),
-                  ),
-                ),
-              ],
-            ),
-          ),
+              child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            future: FirebaseFirestore.instance
+                .collection('user')
+                .doc(user!.uid)
+                .get(),
+            builder: (_, snapshot) {
+              if (snapshot.hasData) {
+                Map<String, dynamic>? data = snapshot.data!.data();
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    /* ----------------- Prifile Image ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey.shade400,
+                        radius: 80,
+                        backgroundImage: NetworkImage(
+                            FirebaseAuth.instance.currentUser?.photoURL ??
+                                "assets/images/Memoji.png"),
+                      ),
+                    ),
+                    /* ----------------- Username ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Text(
+                          FirebaseAuth.instance.currentUser?.displayName ??
+                              'DisplayName',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 27,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black)),
+                    ),
+                    /* ----------------- Address ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 5, 20, 0),
+                      child: Text("Thung khru, Bangkok.",
+                          style: GoogleFonts.montserrat(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black)),
+                    ),
+                    /* ----------------- Show Email ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Column(
+                        children: [
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: textLabel('Email')),
+                          emailBox(data!['email']),
+                        ],
+                      ),
+                    ),
+                    /* ----------------- Edit Phone ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Column(
+                        children: [
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: textLabel('Phone')),
+                          phoneBox(data['Phone']),
+                        ],
+                      ),
+                    ),
+                    /* ----------------- Edit Other Contact ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Column(
+                        children: [
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: textLabel('Other Contact (Optional)')),
+                          otherContactsBox(data['Others Contact']),
+                        ],
+                      ),
+                    ),
+                    /* ----------------- Save Button ---------------- */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 150),
+                      child: SizedBox(
+                        height: 55.0,
+                        child: saveButton(),
+                      ),
+                    ),
+                  ],
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          )),
         ),
       ),
     );
